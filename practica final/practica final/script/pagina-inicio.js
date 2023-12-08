16e457a68b18036c22985e6de0412e91313455cd
macij1 <124784259+macij1@users.noreply.github.com>
2023-12-08 12:44:00 +0100

    Pestaña de cookies y variables css

---
+++ practica final/practica final/script/pagina-inicio.js
@@ -0,0 +1,97 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+    const grande1 = document.querySelector(`.grande1`)
+    const punto1 = document.querySelectorAll('.punto1')
+    const grande2 = document.querySelector(`.grande2`)
+    const punto2 = document.querySelectorAll('.punto2')
+    const grande3 = document.querySelector(`.grande3`)
+    const punto3= document.querySelectorAll('.punto3')
+    var pos1 = 0;
+    var desplazamiento1= 0;
+    var pos2 = 0;
+    var desplazamiento2= 0;
+    var pos3 = 0;
+    var desplazamiento3= 0;
+
+    localStorage.setItem('tema', "default");
+    
+  
+    // Al hacer click en un punto
+    //Saber la pos del punto
+    //Aplicar un transform al grande
+    punto1.forEach( (cadaPunto1, i)=> {
+      punto1[i].addEventListener('click', ()=>{
+      console.log("CLICK1")
+      if(i == 0 && pos1<0){
+        desplazamiento1 = pos1+25
+      }else if(i==1 && pos1>-75){
+        desplazamiento1 = pos1-25
+      }
+      pos1 = desplazamiento1;
+      grande1.style.transform = `translateX(${desplazamiento1}%)`
+      });
+    });
+  
+  
+    punto2.forEach( (cadapunto2, i)=> {
+      punto2[i].addEventListener('click', ()=>{
+      console.log("CLICK2")
+      if(i == 0 && pos2<0){
+        desplazamiento2 = pos2+25
+      }else if(i==1 && pos2>-75){
+        desplazamiento2 = pos2-25
+      }
+      pos2 = desplazamiento2;
+      grande2.style.transform = `translateX(${desplazamiento2}%)`
+      });
+    });
+  
+    punto3.forEach( (cadapunto3, i)=> {
+      punto3[i].addEventListener('click', ()=>{
+      console.log("CLICK3")
+      if(i == 0 && pos3<0){
+        desplazamiento3 = pos3+25
+      }else if(i==1 && pos3>-75){
+        desplazamiento3 = pos3-25
+      }
+      pos3 = desplazamiento3;
+      grande3.style.transform = `translateX(${desplazamiento3}%)`
+      });
+    });
+});
+
+
+const color_fondo1="#ca787333";
+const color_fondo2="#EEB8B5";
+const color_fondo3="#ca7873cc";
+const color_fondo1_alt="#ff0000";
+const color_fondo2_alt="#FFFFFF";
+const color_fondo3_alt="#000000e6";
+
+  function ocultarFooter() {
+    document.querySelector(".footer").style.display = "none";
+    document.querySelector(".cookies-button").style.opacity =  1;
+  }
+
+  function mostrarFooter(){
+    document.querySelector(".footer").style.display = "block";
+    document.querySelector(".cookies-button").style.opacity = 0
+  }
+
+  function changeTheme(){
+    var tema = localStorage.getItem('tema');
+    if(tema == "default"){
+      localStorage.setItem('tema', "high_contrast");
+      document.documentElement.style.setProperty("--color-fondo1", color_fondo1_alt);
+      document.documentElement.style.setProperty("--color-fondo2", color_fondo2_alt);
+      document.documentElement.style.setProperty("--color-fondo3", color_fondo3_alt);
+    }else{
+      localStorage.setItem('tema', "default");
+      document.documentElement.style.setProperty("--color-fondo1", color_fondo1);
+      document.documentElement.style.setProperty("--color-fondo2", color_fondo2);
+      document.documentElement.style.setProperty("--color-fondo3", color_fondo3);
+    }
+
+  }
+
+
